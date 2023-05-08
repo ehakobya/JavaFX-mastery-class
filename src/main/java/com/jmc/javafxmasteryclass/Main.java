1b56fd9a62d163fd0acf0bf493e3cd8b350ba58e
Edgar Hakobyan <ehakobya@asu.edu>
2023-05-08 03:23:40 -0700

    Add observable collections demo

--- src/main/java/com/jmc/javafxmasteryclass/Main.java
+++ src/main/java/com/jmc/javafxmasteryclass/Main.java
@@ -1,22 +1,41 @@
 package com.jmc.javafxmasteryclass;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
         VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
         root.setSpacing(10);
+        ObservableList<String> list = FXCollections.observableArrayList("One", "Two", "Three");
 
+        ListView<String> items = new ListView<>();
 
-        root.getChildren().addAll();
-        Scene scene = new Scene(root, 500, 500);
+        TextField field = new TextField();
+        field.setMaxWidth(200);
+
+        Button button = new Button("Add");
+
+        items.setItems(list);
+        button.setOnAction(actionEvent -> {
+            if (!field.textProperty().get().isEmpty()) {
+                list.add(field.getText());
+            }
+        });
+
+        root.getChildren().addAll(items, field, button);
+
+        Scene scene = new Scene(root, 550, 500);
         stage.setScene(scene);
         stage.show();
     }
