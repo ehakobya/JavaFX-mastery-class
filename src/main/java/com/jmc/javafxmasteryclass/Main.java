b670f05169f9a44aa7d92981d37cd7abfc7a0c74
Edgar Hakobyan <ehakobya@asu.edu>
2023-05-09 22:21:23 -0700

    Add FlowPane Demo

--- src/main/java/com/jmc/javafxmasteryclass/Main.java
+++ src/main/java/com/jmc/javafxmasteryclass/Main.java
@@ -1,23 +1,42 @@
 package com.jmc.javafxmasteryclass;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        VBox root = new VBox();
-        root.setAlignment(Pos.CENTER);
-        root.setSpacing(10);
+        // used for series of objects (e.g. pictures) that are displayed next to each other
+        FlowPane flowPane = new FlowPane();
+        flowPane.setOrientation(Orientation.VERTICAL);
+        flowPane.setHgap(30);
+        flowPane.setVgap(10);
+        // flowPane.setRowValignment(VPos.BOTTOM);
+        flowPane.setColumnHalignment(HPos.RIGHT);
 
+        Button b1 = new Button("Button 1");
+        Button b2 = new Button("Button 2");
+        Button b3 = new Button("Button 3");
+        TextArea textArea = new TextArea();
+        Button b4 = new Button("Button 4");
+        Button b5 = new Button("Button 5");
+        Button b6 = new Button("Button 6");
 
-        root.getChildren().addAll();
-        Scene scene = new Scene(root, 500, 500);
+        flowPane.getChildren().addAll(b1, b2, b3, textArea, b4, b5, b6);
+
+        Scene scene = new Scene(flowPane, 500, 450);
+
         stage.setScene(scene);
+        stage.setTitle("Flow Pane Example");
         stage.show();
     }
 
